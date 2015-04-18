a451e931cbfbfe3c36b494407bb976471ce0f3be
Dylan Wulf <wulfd1@tcnj.edu>
2015-04-18 18:38:23 -0400

    Added makefile to git

---
+++ Makefile
@@ -0,0 +1,22 @@
+INC = `wx-config --cppflags` -I./include
+LIBS = `wx-config --static --libs`
+BD = build
+ID = include
+SRC = src
+
+$(BD)/EasyTourney: $(BD)/TourneyApp.o $(BD)/TourneyFrame.o
+	g++ $? -o $@ $(LIBS)
+
+$(BD)/TourneyApp.o: $(SRC)/TourneyApp.cpp $(ID)/TourneyApp.h $(ID)/TourneyFrame.h
+	g++ $(INC) -c $< -o $@
+
+$(BD)/TourneyFrame.o: $(SRC)/TourneyFrame.cpp $(ID)/TourneyFrame.h
+	g++ $(INC) -c $< -o $@
+
+.PHONY: clean
+
+clean:
+	rm -f $(BD)/*
+
+run: $(BD)/EasyTourney
+	$<
---
+++ src/TourneyFrame.cpp
@@ -0,0 +1,28 @@
+/**************************************************************
+EasyTourney -- A simple, easy to use tournament bracket manager
+Copyright (C) 2015 Dylan Wulf
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+****************************************************************/
+
+#include <TourneyFrame.h>
+
+TourneyFrame::TourneyFrame(const wxString& title, const wxPoint& pos, const wxSize& size): wxFrame(NULL, wxID_ANY, title, pos, size) {
+
+}
+
+void TourneyFrame::OnQuit(){
+
+}
